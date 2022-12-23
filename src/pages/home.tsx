a1a525f8c70b7b861b92e3fc8c7fc59ace50ded3
soheilghanbary <soli.ghanbary@gmail.com>
2022-12-23 20:38:08 +0330

    update vite routing

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,11 @@
+import { useNavigate } from "react-router-dom"
+
 export default function Home() {
+    const navigate = useNavigate()
     return (
         <div>
             <h1>this is my home page</h1>
+            <button onClick={() => navigate('/users')}>Go to Users</button>
         </div>
     )
 }
